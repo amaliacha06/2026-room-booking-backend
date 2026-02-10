acb58f1196dd76113c6f790915cc9de1041d1041
amaliacha06 <chasanahzahty@gmail.com>
2026-02-11 00:07:05 +0700

    chore(db): migrate booking table to database

---
+++ Migrations/20260210161558_AddBookingTable.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace room_booking_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Bookings",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Bookings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Purpose",
+                table: "Bookings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Bookings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Bookings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Users_UserId",
+                table: "Bookings",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Users_UserId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Purpose",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Bookings");
+        }
+    }
+}
